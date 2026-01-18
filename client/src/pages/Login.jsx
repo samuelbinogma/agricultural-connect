052c03274ba0c1bbd281dff25f0a073328c4aadb
samuelbinogma <sbinogma41@gmail.com>
2026-01-18 10:12:43 +0000

    create the Login page

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,19 @@
+// src/pages/Login.jsx
+import AuthForm from '../components/AuthForm';
+
+export default function Login() {
+    const handleLogin = (data) => {
+        console.log('Login attempt:', data);
+        // TODO: send to backend
+        alert('Login submitted! (demo)');
+    };
+
+    return (
+        <AuthForm 
+        type="login" 
+        // You can pass role from URL/query later
+        // role="farmer" or "customer"
+        onSubmit={handleLogin} 
+        />
+    );
+}
