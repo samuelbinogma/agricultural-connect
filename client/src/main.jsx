eebfdf6304f9247a1146d445b975d3d8ff429263
samuelbinogma <sbinogma41@gmail.com>
2026-01-18 19:48:14 +0000

    install react router dom

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './styles/main.css'
 import './styles/auth.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
 )
