6406e93e5b98df1a8e5ca82309bfd262af9911fc
samuelbinogma <sbinogma41@gmail.com>
2026-02-01 18:57:08 +0000

    create product model

---
+++ server/src/models/Product.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    farmer: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+
+    name: {
+        type: String,
+        required: [true, 'Product name is required'],
+        trim: true
+    },
+
+    description: {
+        type: String,
+        trim: true
+    },
+
+    price: {
+        type: Number,
+        required: [true, 'Price is required'],
+        min: 0
+    },
+
+    unit: {
+        type: String,
+        enum: ['kg', 'piece', 'bunch', 'crate', 'basket'],
+        default: 'kg'
+    },
+
+    quantity: {
+        type: Number,
+        required: [true, 'Quantity is required'],
+        min: 1
+    },
+
+    category: {
+        type: String,
+        enum: ['vegetables', 'fruits', 'tubers', 'grains', 'others'],
+        required: true
+    },
+
+    imageUrl: {
+        type: String, 
+        default: ''
+    },
+
+    createdAt: { type: Date, default: Date.now},
+    updatedAt: { type: Date, default: Date.now}
+});
+
+productSchema.pre('save', function(next) {
+    this.updatedAt = Date.now();
+    next();
+})
+
+module.exports = mongoose.model('Product', productSchema);
